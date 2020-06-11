f0e6e719711da23cc573a0adccd47c79f35f118e
Pablo Stebler <pablo@stebler.xyz>
2020-06-11 12:23:33 +0200

    Revert "mb/dell/latitude_d620: Serial port initialization"
    This reverts commit 9b07c101e8ddb4bae1fcac57b8dfa86ba981c818.

--- src/mainboard/dell/latitude_d620/early_init.c
+++ src/mainboard/dell/latitude_d620/early_init.c
@@ -1,15 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
-#include <bootblock_common.h>
 #include <northbridge/intel/i945/i945.h>
-#include <superio/smsc/smscsuperio/smscsuperio.h>
-
-#define SERIAL_DEV PNP_DEV(0x2e, SMSCSUPERIO_SP1)
-
-void bootblock_mainboard_early_init(void)
-{
-        smscsuperio_enable_serial(SERIAL_DEV, CONFIG_TTYS0_BASE);
-}
 
 void mainboard_late_rcba_config(void)
 {
